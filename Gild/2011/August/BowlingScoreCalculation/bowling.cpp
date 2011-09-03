c18e358ba87ca124deae1cef4451cdb8bc213ae2
Samkit Jain <samjam86@yahoo.com>
2011-09-03 10:39:00 +0530

    More reduction

--- Gild/2011/August/BowlingScoreCalculation/bowling.cpp
+++ Gild/2011/August/BowlingScoreCalculation/bowling.cpp
@@ -2,31 +2,6 @@
 // Code: https://github.com/samjam86/coding-competition/blob/master/Gild/2011/August/BowlingScoreCalculation/bowling.cpp
 #include <cstdio>
 
-short getScore(char const* line, short* valueMap, short* bonusMap)
-{
-    short score = 0;
-    for (short i = 0, frames = 0, attempts = 0, c; frames < 10; i += 2)
-    {
-        // this value
-        c = valueMap[line[i]];
-
-        // look ahead by 1
-        valueMap['/'] = 10 - (bonusMap['/'] = valueMap[line[i + 2]]);
-
-        // look ahead by 2
-        bonusMap['X'] = bonusMap['/'] + valueMap[line[i + 4]];
-
-        score += c + bonusMap[line[i]];
-
-        valueMap['/'] = 10 - c;
-
-        frames += (((line[i] == '/') || (line[i] == 'X')) | (++attempts == 2)) & 0x1;
-        attempts = !((attempts & 0x2) | ((line[i] == '/') || (line[i] == 'X')));
-    }
-
-    return score;
-}
-
 int main(int argc, const char *argv[])
 {
     short valueMap['Z'] = { 0x0 }, bonusMap['Z'] = { 0x0 };
@@ -42,6 +17,26 @@ int main(int argc, const char *argv[])
     char* line = 0;
     for (size_t len, readCount; (readCount = getline(&line, &len, inFile)) != -1 && readCount > 1;)
     {
-        printf("%d\n", getScore((line[readCount + 1] = 'A', line[readCount + 3] = 'A', line), valueMap, bonusMap));
+        line[readCount + 1] = 'A', line[readCount + 3] = 'A';
+        short score = 0;
+        for (short i = 0, frames = 0, attempts = 0, c; frames < 10; i += 2)
+        {
+            // this value
+            c = valueMap[line[i]];
+
+            // look ahead by 1
+            valueMap['/'] = 10 - (bonusMap['/'] = valueMap[line[i + 2]]);
+
+            // look ahead by 2
+            bonusMap['X'] = bonusMap['/'] + valueMap[line[i + 4]];
+
+            score += c + bonusMap[line[i]];
+
+            valueMap['/'] = 10 - c;
+
+            frames += (((line[i] == '/') || (line[i] == 'X')) | (++attempts == 2)) & 0x1;
+            attempts = !((attempts & 0x2) | ((line[i] == '/') || (line[i] == 'X')));
+        }
+        printf("%d\n", score);
     }
 }
