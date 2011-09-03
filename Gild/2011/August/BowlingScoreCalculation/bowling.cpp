2706026ff450a0854fdd8caec7d629bea157781f
Samkit Jain <samjam86@yahoo.com>
2011-09-03 12:57:28 +0530

    Add assumption

--- Gild/2011/August/BowlingScoreCalculation/bowling.cpp
+++ Gild/2011/August/BowlingScoreCalculation/bowling.cpp
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <cstdlib>
 
+// BTW there are many many assumptions made in this program,
+// like the length of the line cannot exceed 100 and read
+// bytes cannot overflow short etc.
 int main(int argc, const char *argv[])
 {
     short valueMap['Z' - '/'] = { 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, bonusMap['Z' - '/'] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -10,9 +13,9 @@ int main(int argc, const char *argv[])
 
     FILE* inFile = fopen(argv[1], "r");
 
-    char* readLine = (char*)malloc(200);
+    char* readLine = (char*)malloc(100);
     short readCount, score;
-    for (size_t len = 200; (readCount = getline(&readLine, &len, inFile)) > 1; score = 0)
+    for (size_t len = 100; (readCount = getline(&readLine, &len, inFile)) > 1; score = 0)
     {
         readLine[readCount + 1] = readLine[readCount + 3] = 'A';
 
@@ -20,15 +23,11 @@ int main(int argc, const char *argv[])
         for (short frames = 0, attempts = 0, c; frames < 10; line += 2)
         {
             c = valueMap[*line - '/'];
-            bonusMap[0] = valueMap[line[2] - '/'];
-            valueMap[0] = 10 - bonusMap[0];
+            valueMap[0] = 10 - (bonusMap[0] = valueMap[line[2] - '/']);
             bonusMap['X' - '/'] = bonusMap[0] + valueMap[line[4] - '/'];
             score += c + bonusMap[*line - '/'];
             valueMap[0] = 10 - c;
-            attempts = (++attempts != 2);
-            attempts &= (*line != 'X');
-            attempts &= (*line != '/');
-            frames += !attempts;
+            frames += !(attempts = !((++attempts == 2) | ((*line == 'X') || (*line == '/'))));
         }
         printf("%d\n", score);
     }
