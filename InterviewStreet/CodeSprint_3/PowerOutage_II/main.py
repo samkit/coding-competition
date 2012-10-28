53965d89d5842cf09945391fcb241d788755ccf8
Samkit Jain <samkit@Connexion-Exp.(none)>
2012-10-28 19:41:15 +0530

    Successful attempt

--- InterviewStreet/CodeSprint_3/PowerOutage_II/main.py
+++ InterviewStreet/CodeSprint_3/PowerOutage_II/main.py
@@ -28,6 +28,28 @@ def path_length(grid, path):
         previous = i
     return length
 
+def less_cost_comparator(x, y):
+    if x[1] is None:
+        return 1
+    if y[1] is None:
+        return -1
+    return x[1] - y[1]
+
+def all_combinations(G, L, grid, level, deployed, costTillNow, minCost, *path):
+    if level == len(grid):
+        minCost[0] = min(minCost[0], costTillNow) if minCost[0] else costTillNow
+        minCost[1] = path
+        return
+
+    for i in grid[level]:
+        cost = L[level] * (i[1] if i[1] else 0)
+        if i[0] not in [ z[1] for z in deployed.iteritems() ]:
+            deployed[level] = i[0]
+            cost += G[i[0]]
+        if not (minCost[0] and minCost[0] < costTillNow + cost):
+            all_combinations(G, L, grid, level + 1, deployed, costTillNow + cost, minCost, [ level, i[0] ], *path)
+        deployed[level] = None
+
 inFile = sys.stdin
 
 N, M = [ int(i) for i in inFile.readline().split() ]
@@ -62,20 +84,20 @@ for generator in xrange(N):
 
         costGrid[generator][pole] = path_length(grid, minPath) * L[pole] if minPath else None
 
-pprint.pprint(costGrid)
-
+sortedGrids = dict()
 for pole in xrange(N):
     costs = map(lambda x: [ x[0], x[1][pole] ], costGrid.iteritems())
-    print pole, costs
-    preferred = list()
+    costs.sort(cmp = less_cost_comparator)
+    sortedGrids[pole] = costs
 
-    def reducer(x, y):
-        if x is None:
-            return y
-        if y is None:
-            return x
-        if x < y:
-            return x
-        return y
-#   minCost = reduce(reducer, costs)
-#   print pole, costs, '=====', minCost
+minCost = [ None, None ]
+all_combinations(G, L, sortedGrids, 0, {}, 0, minCost)
+
+generators = set([ z[1] for z in minCost[1] ])
+print len(generators)
+for i in generators:
+    print i + 1,
+print
+for i in sorted(minCost[1], key = lambda x: x[0]):
+    print i[1] + 1,
+print
